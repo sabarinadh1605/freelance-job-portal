44acc9e2c3728ce6da1677a75cacc70fbdd83dc0
Sabarinadh M <111140745+sabarinadh1605@users.noreply.github.com>
2026-09-03 16:59:07 +0530

    Add files via upload

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // Your Supabase URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // Your Supabase anon key
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
